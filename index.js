ea168263a3f0a29a70925d4d8968556c6787217f
Logan Smith <logsmith06@gmail.com>
2020-12-15 19:07:44 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function wakeDog(dogName, dogBreed){
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+}
+
+function leashDog(dogName, dogBreed){
+
+}
+
+function walkToPark(dogName, dogBreed){
+
+}
+
+function throwFrisbee(dogName, dogBreed){
+
+}
+
+function walkHome(dogName, dogBreed){
+
+}
+
+function unleashDog(dogName, dogBreed){
+
+}
