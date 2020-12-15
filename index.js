733aa9597d09740d6c6a91d78a8672cbf5f3652c
Logan Smith <logsmith06@gmail.com>
2020-12-15 19:10:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,21 +5,31 @@ function wakeDog(dogName, dogBreed){
 }
 
 function leashDog(dogName, dogBreed){
-
+  let action = `Leash ${dogName} the ${dogBreed}`
+  console.log(action);
+  return action;
 }
 
 function walkToPark(dogName, dogBreed){
-
+  let action = `Walk to the park with ${dogName} the ${dogBreed}`
+  console.log(action);
+  return action;
 }
 
 function throwFrisbee(dogName, dogBreed){
-
+  let action = `Throw the frisbee for ${dogName} the ${dogBreed}`
+  console.log(action);
+  return action;
 }
 
 function walkHome(dogName, dogBreed){
-
+  let action = `Wake ${dogName} the ${dogBreed}`
+  console.log(action);
+  return action;
 }
 
 function unleashDog(dogName, dogBreed){
-
+  let action = `Wake ${dogName} the ${dogBreed}`
+  console.log(action);
+  return action;
 }
